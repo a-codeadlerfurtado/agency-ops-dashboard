09a75bb0342616c2b306ebb1beb2a198ac11f7f5
a-codeadlerfurtado <adlerfurtadomkt01@gmail.com>
2026-08-28 14:36:36 -0300

    style(meta-radar): polish cockpit visualization
    * style(meta-radar): refocus cockpit visual hierarchy
    
    * style(meta-radar): load cockpit polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./dashboard-enhancements.css";
 import "./onboarding-legacy.css";
 import "./motion-system.css";
 import "./contrast-tune.css";
+import "./meta-radar/radar-polish.css";
 import IntegrationHealthBar from "./integration-health-bar";
 import CampaignsNavBridge from "./campaigns-nav-bridge";
 import CampaignNotesBridge from "./campaign-notes-bridge";
