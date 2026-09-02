dcbe5945ac65d88a548c65681de2153d8a012ad8
a-codeadlerfurtado <adlerfurtadomkt01@gmail.com>
2026-09-02 11:21:46 -0300

    chore: remove unused Briefing Hub demo launcher

--- app/briefing-hub-demo-launcher.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import BriefingHubDemoLink from "./briefing-hub-demo-link";
-
-export default function BriefingHubDemoLauncher(){
-  return <div style={{position:"fixed",right:18,bottom:84,zIndex:9000}}><BriefingHubDemoLink/></div>;
-}
