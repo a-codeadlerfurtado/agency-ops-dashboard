29798c55368153b148e0f885ca3ec67f4096fb10
a-codeadlerfurtado <adlerfurtadomkt01@gmail.com>
2026-09-25 18:42:35 -0300

    chore(relato): bump desktop agent to 0.5.2

--- desktop-agent/RelatoAI.DesktopAgent/AgentUpdater.cs
+++ desktop-agent/RelatoAI.DesktopAgent/AgentUpdater.cs
@@ -6,7 +6,7 @@ namespace RelatoAI.DesktopAgent;
 
 internal sealed class AgentUpdater : IDisposable
 {
-    public const string CurrentVersion = "desktop-0.5.1";
+    public const string CurrentVersion = "desktop-0.5.2";
     private readonly Func<AgentConfig?> configProvider;
     private readonly Action<string> status;
     private readonly Action exitAgent;
